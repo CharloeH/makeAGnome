039b668bb78c8226e3459fb7b69f2fb7e4dc682c
Sebastian Horton <36234258+SebastianH11@users.noreply.github.com>
2018-04-26 11:22:49 -0400

    Update MainWindow.xaml.cs
    day 2 ver1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,22 +20,31 @@ namespace makeAGnome
     /// </summary>
     public partial class MainWindow : Window
     {
+        Ellipse e1 = new Ellipse();
         public MainWindow()
         {
+           
             InitializeComponent();
+            CreatePlayer();
         }
 
         private void btnChangeControls_Click(object sender, RoutedEventArgs e)
         {
             ChangeControls ChangeControls = new ChangeControls();
             bool? dialogResult = ChangeControls.ShowDialog();
-            if(dialogResult == true)
+            if (dialogResult == true)
             {
                 MessageBox.Show(ChangeControls.getValue());
             }
-     }
-
-        private void CreatePlayer()
+        }
+        private void PlaySpace_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.W)
+            {
+                Canvas.SetTop(e1, 100);
+            }
+        }
+        public Ellipse CreatePlayer()
         {
             Ellipse e1 = new Ellipse();
             e1.Height = 20;
@@ -44,6 +53,17 @@ namespace makeAGnome
             Canvas.SetTop(e1, 20);
             Canvas.SetLeft(e1, 20);
             PlaySpace.Children.Add(e1);
+            return e1;
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.W)
+            {
+                MessageBox.Show("ay");
+                Canvas.SetTop(e1, 100);
+                e1.UpdateLayout();
+            }
         }
     }
 }
